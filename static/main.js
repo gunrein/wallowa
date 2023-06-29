52b1fc3c72ff197599aee71d62fedf4be42ccb4a
Greg Unrein <greg@unre.in>
2023-06-29 14:47:35 -0700

    Update nav menu event handling to use htmx
    Also fixed htmx usage

--- static/main.js
+++ static/main.js
@@ -1,1 +1,12 @@
-import * as htmx from "/static/htmx-1.9.2.min.js";
+import "/static/htmx-1.9.2.min.js";
+
+window.htmx = htmx;
+
+htmx.onLoad(function(elt) {
+  // Navigation menu toggle
+  htmx.find('#navbar-toggle').addEventListener('change', function(ev) {
+    elt.findAll.querySelectorAll('.menu-item-hidden-small').forEach((node) => {
+      node.classList.toggle('hidden');
+    });
+  });
+});
