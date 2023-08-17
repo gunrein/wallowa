f4c350257ff49337cb562977f250027d4c602aa1
Greg Unrein <greg@unre.in>
2023-08-17 10:42:16 -0700

    Fix query to correctly handle multiple repo timeseries

--- src/github/queries.rs
+++ src/github/queries.rs
@@ -113,15 +113,15 @@ pub fn merged_pr_duration_rolling_daily_average(
     let conn = pool.get()?;
 
     let repo_placeholders = if repos.is_empty() {
-        "".to_string()
+        "SELECT DISTINCT (row.base.repo.owner.login || '/' || row.base.repo.name) AS repo FROM pulls".to_string()
     } else {
         let mut placeholders = "?,".repeat(repos.len());
         placeholders.pop(); // Remove the trailing comma (`,`)
-        format!("AND repo IN ({})", placeholders)
+        format!("SELECT unnest([{}]) AS repo", placeholders)
     };
+    debug!("repo_placeholders: {:?} for {:?}", repo_placeholders, repos);
 
-    let mut stmt = conn.prepare(
-        &format!(r#"
+    let mut stmt = conn.prepare(&format!(r#"
 -- merged_pr_duration_rolling_daily_average
 -- Duration of merged GitHub Pull Requests, rolling daily average
 WITH calendar_day AS (
@@ -147,30 +147,48 @@ pulls AS (
                 "created_at": "TIMESTAMP",
                 "closed_at": "TIMESTAMP",
                 "merged_at": "TIMESTAMP",
+                "updated_at": "TIMESTAMP",
                 "draft": "BOOLEAN"
             }}]')) AS row,
     FROM wallowa_raw_data
     WHERE "data_source" = 'github_rest_api'
     AND data_type = 'pulls'
 ),
-rolling AS (
-    SELECT DISTINCT ON (row.url)
-        (row.base.repo.owner.login || '/' || row.base.repo.name) AS repo,
-        CAST(row.created_at AS DATE) AS created_date,
-        CAST(row.merged_at AS DATE) AS merged_date,
-        AVG(EPOCH(AGE(row.merged_at, row.created_at)) / 86400) OVER thirty AS duration
-    FROM pulls
-    WHERE row.merged_at NOT NULL
+repos AS (
     {repo_placeholders}
+),
+calendar_day_repos AS (
+    -- Generate a series of days for each repo so that each day+repo has a rolling average represented
+    SELECT calendar_day."day", repos.repo FROM calendar_day CROSS JOIN repos
+),
+latest_deduped_pulls AS (
+    SELECT
+        row.url AS "url",
+        (row.base.repo.owner.login || '/' || row.base.repo.name) AS repo,
+        row.created_at AS created_at,
+        row.merged_at AS merged_at,
+        row.updated_at AS updated_at,
+        row_number() OVER (PARTITION BY "url" ORDER BY updated_at DESC) AS row_number
+    FROM pulls
+    WHERE repo IN (SELECT repo FROM repos)
+),
+rolling AS (
+    SELECT
+        repo,
+        CAST(created_at AS DATE) AS created_date,
+        CAST(merged_at AS DATE) AS merged_date,
+        AVG(EPOCH(AGE(merged_at, created_at)) / 86400) OVER thirty AS duration
+    FROM latest_deduped_pulls
+    WHERE row_number = 1
+    AND merged_at NOT NULL
     WINDOW thirty AS (
         PARTITION BY repo
-        ORDER BY row.created_at ASC
+        ORDER BY created_at ASC
         RANGE BETWEEN INTERVAL 30 DAYS PRECEDING
                 AND INTERVAL 0 DAYS FOLLOWING)
 )
-SELECT calendar_day."day" AS "day", rolling.repo, AVG(rolling.duration) AS "duration"
-FROM calendar_day
-ASOF LEFT JOIN rolling ON calendar_day."day" >= rolling.merged_date
+SELECT calendar_day_repos."day" AS "day", rolling.repo, AVG(rolling.duration) AS "duration"
+FROM calendar_day_repos ASOF LEFT JOIN rolling ON (calendar_day_repos.repo = rolling.repo AND calendar_day_repos."day" >= rolling.merged_date)
 GROUP BY 1,2
 ORDER BY 1,2
 "#, repo_placeholders = repo_placeholders))?;
